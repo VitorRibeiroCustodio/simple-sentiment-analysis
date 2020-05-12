7f96ea48080a5c6e6e59ee9174d35169b686c258
VitorRibeiroCustodio <vitorribeiro221196@gmail.com>
2020-05-12 17:32:54 -0300

    Readme and initial structure

---
+++ Readme.md
@@ -0,0 +1,13 @@
+#Simple Sentiment analysis POC
+
+ - Use NTLK to build a simple identifier of sentiments
+
+
+Requirements:
+
+ - pip install nltk==3.3
+ - nltk.download('twitter_samples')
+ - nltk.download('punkt')
+ - nltk.download('wordnet')
+ - nltk.download('averaged_perceptron_tagger')
+ - nltk.download('stopwords')
---
+++ nlp_test.py
@@ -0,0 +1,52 @@
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import twitter_samples
+import re, string
+from nltk.corpus import stopwords
+
+
+# Downloading the Data
+positive_tweets = twitter_samples.strings('positive_tweets.json')
+negative_tweets = twitter_samples.strings('negative_tweets.json')
+text = twitter_samples.strings('tweets.20150430-223406.json')
+stop_words = stopwords.words('english')
+
+# Tokenizing the Data
+tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+
+# Remove 'Noise' from the Data
+def remove_noise(tweet_tokens, stop_words = ()):
+
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
+        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
+
+positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+
+positive_cleaned_tokens_list = []
+negative_cleaned_tokens_list = []
+
+for tokens in positive_tweet_tokens:
+    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
+for tokens in negative_tweet_tokens:
+    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
